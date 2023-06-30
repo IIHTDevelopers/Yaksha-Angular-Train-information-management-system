55f66941f2fd22f5523b4f78ff0a603b70bca6f0
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-06-30 07:56:01 +0530

    Add files via upload

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TrainComponent } from './component/train/train.component';
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [    
+        RouterTestingModule ,FormsModule,ReactiveFormsModule
+      ],
+      declarations: [AppComponent,
+        TrainComponent],
+      providers:[HttpClient,HttpHandler]
+
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  describe('business',()=>{
+        it('should create app component', () => {
+          expect(component).toBeTruthy();
+        });
+
+        it('should have title as Train Information Management System App', () => {
+          const titleElement = fixture.nativeElement.querySelector('h1');
+          expect(titleElement.textContent).toContain('Train Information Management System App');
+        });
+
+      });
+
+});
